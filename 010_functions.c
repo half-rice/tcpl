be02571a7dcda2fcdaea5b0b65b35c487b638db9
surrus <xsurrus@gmail.com>
2015-09-14 08:21:41 -0600

    010_functions.c

---
+++ 010_functions.c
@@ -0,0 +1,23 @@
+#include  <stdio.h>
+
+int  power(int  m,  int  n);
+
+/*  test  power  function  */
+main(){
+  int  i;
+  for(i = 0; i < 10; ++i)
+    /* printf("%d %d %d\n", i, power(2, i), power(-3, i)); */
+    printf("4^%d = %d\n", i, power(4, i));
+}
+
+/* power: raise base to n-th power; n >= 0 */
+int power(int base, int n){
+  int i, p;
+
+  p = 1;
+  for(i = 1; i <= n; ++i){
+    p = p * base;
+    printf("p = %d, ", p);
+  }
+  return p;
+}
