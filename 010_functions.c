ad96e1784e689a78610824c11630d97b359d8299
surrus <xsurrus@gmail.com>
2015-09-14 21:19:41 -0600

    formatting fix

--- 010_functions.c
+++ 010_functions.c
@@ -1,8 +1,8 @@
 #include  <stdio.h>
 
-int  power(int  m,  int  n);
+int power(int m, int n);
 
-/*  test  power  function  */
+/*  test power function */
 main(){
   int  i;
   for(i = 0; i < 10; ++i)
