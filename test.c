02d137e3ad55e36a8f5aa234dbdb7f7411c85719
surrus <xsurrus@gmail.com>
2015-09-17 07:51:01 -0600

    test.c

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+  int array[10];
+  int count;
+
+  count = 0;
+  int i;
+  for (i=0; i<10; ++i){
+    array[i] = 0 + (++count);
+    printf("%d\n", array[i]);
+
+  }
+
+  // printf("%d\n", array);
+
+  return(0);
+}
