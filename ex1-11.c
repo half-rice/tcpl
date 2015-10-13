ff77ecd9073653e1bf01b6884bb492a32e5982cf
surrus <xsurrus@gmail.com>
2015-10-13 16:42:00 -0500

    ex1-11.c

---
+++ ex1-11.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+#define IN  1     /* inside a word */
+#define OUT 0     /* outside a word */
+
+/* count lines, words, and chars in input */
+main(){
+  int c, nl, nw, nc, state;
+  char x[100] = "wtf-haha()!@^#*> o <> no???\ttest\n\n";
+
+  state = OUT;
+  nl = nw = nc = 0;
+
+  int i;
+  for(i = 0; i <= strlen(x); ++i){ 
+    ++nc;
+    if(c == '\n')
+      ++nl;
+    if(c == ' ' || c == '\n' || c == '\t')
+      state = OUT;
+    else if(state == OUT){
+      state = IN;
+      ++nw;
+    }
+  }
+  printf("%d %d %d\n", nl, nw, nc);
+}
