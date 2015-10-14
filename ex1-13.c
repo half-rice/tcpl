7bb976164a9a9824b74d57e1158d4f72a00379b0
surrus <xsurrus@gmail.com>
2015-10-13 20:03:23 -0500

    ex1-13.c

--- ex1-13.c
+++ ex1-13.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 
+#define IN  1
+#define OUT 0
+
 /* Write a program to print a histogram of the lengths of words in its 
    input. It is easy to draw the histogram with the bars horizontal; a
    vertical orientation is more challenging */
 int main(){
-  int c;
+  int c, len, state;
+  int wordcount;
+  int wordlen[20];
 
+  int i;
+  for(int i = 0; i < 20; ++i)
+    wordlen[i] = 0;
+
+  state = OUT;
+  wordcount = 0;
   while((c = getchar()) != EOF){
-    
+    if(c >= 65 || c <= 90 || c >= 97 || c <= 122 && state == OUT){
+      state = IN;
+      ++len;
+    }
+    else if(c == ' ' || c == '\n' || c == '\t'){
+      state = OUT;
+    }
   }
 
   return 0;
