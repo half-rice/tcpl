24893592dd7816fb28eeb4100da441a4296a6bac
surrus <xsurrus@gmail.com>
2015-10-13 19:44:20 -0500

    ex1-13.c

---
+++ ex1-13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* Write a program to print a histogram of the lengths of words in its 
+   input. It is easy to draw the histogram with the bars horizontal; a
+   vertical orientation is more challenging */
+int main(){
+  int c;
+
+  while((c = getchar()) != EOF){
+    
+  }
+
+  return 0;
+}
