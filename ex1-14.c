a8f80e429ef90c304991d1ff680a06fbb663f1ea
surrus <xsurrus@gmail.com>
2015-10-14 20:26:44 -0500

    ex1-14.c

--- ex1-14.c
+++ ex1-14.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
 int main(){
+  int c, i;
+  char cc;
+  int alpha[27];
+
+for(i = 0; i < 26; ++i)
+  alpha[i] = 0;
+
+/* Write a program to print a histogram of the frequencies of 
+   different characters in its input */
+  while((c = getchar()) != EOF){
+    for(i = 0; i < 26; ++i){
+      if(c - 'a' == i){
+        alpha[i] += 1;
+      }
+    }
+  }
+
+  for(i = 0; i < 26; ++i){
+    printf("%d ", alpha[i]);
+  }
+  printf("\n");
+  for(i = 0; i < 26; ++i){
+    char cc = i + 'a';
+    printf("%c ", cc);
+  }
+  printf("\n");
 
   return 0;
 }
