1d6140b0a533e20d129208421a5cdce5b6a3a809
surrus <xsurrus@gmail.com>
2015-11-02 13:29:30 -0700

    trim_line(), save_line() tested

--- ex1-18.c
+++ ex1-18.c
@@ -1,13 +1,12 @@
-
 #include <stdio.h>
 
-#define MAXLINE 1000      /* maximum input line size */
+#define MAXLINE 1000        /* maximum input line size */
 
-int lineLen(char line[], int lim);
-void saveLine(char line[], int len);
+int line_len(char line[], int lim);
+void save_line(char line[], int len);
 int trim_line(char line[], int len);
   
-char lines[10000];        /* saved lines */
+char lines[10000];          /* saved lines */
 int lines_counter;
 
 /* Write a program to remove trailing blanks and tabs from each 
@@ -23,8 +22,7 @@ int main(){
   lines_counter = 0;
 
   while((len = line_len(line, MAXLINE)) > 0){
-    trim_line(line, len);
-    save_line(line, len);
+    save_line(line, (len-trim_line(line, len)));
   }
 
   printf("lines = \n%s\n", lines);
@@ -32,22 +30,35 @@ int main(){
   return 0;
 }
 
+/* trim_line: read a line into line[], return amount to trim */
 int trim_line(char line[], int len){
   int i;
+  int del_count;
   int done_trim;
 
+  i = len-1;
+  del_count = 0;
   done_trim = 0;
 
-  for(i = len; i > 0; --i){
+  while(done_trim != 1){
     if(line[i] == '\n'){
       done_trim = 0;
     }
-    else if(line[i] == ' ' || line[i] == '\t' && done_trim != 1){
-
+    else if(line[i] == '\0'){
+      printf("\nseein zeros\n");
     }
+    else if(line[i] == ' ' || line[i] == '\t'){
+      printf("\nline[i] %c == ' ' or '\t', ++del_count", line[i]);
+      ++del_count;
+    }
+    else if((line[i] != ' ' || line[i] != '\t' || line[i] != '\0') && i >= 0){
+      line[i+1] = '\n';
+      done_trim = 1;
+    }
+    --i;
   }
 
-  return i;
+  return del_count;
 }
 
 /* line_len: read a line into line[], return length */
@@ -65,15 +76,19 @@ int line_len(char line[], int lim){
   return i;
 }
 
+/* save_line: read a line into line[], append indexes of line[]
+   into lines[] */
 void save_line(char line[], int len){
   int i;
-
   extern char lines[];
   extern int lines_counter;
 
   i = 0;
-  while((lines[lines_counter] = line[i]) != '\0' && i <= len){
+
+  while((lines[lines_counter] = line[i]) != '\0' && i < len){
     ++lines_counter;
     ++i;
   }
+  printf("saving line[] into lines[]...\n");
+  printf("lines[]:\n%s\n", lines);
 }
