e3c9cc18df9f29f055ca8a5e4c638a5b244b0f32
surrus <xsurrus@gmail.com>
2015-11-06 17:10:43 -0700

    entab() counts spaces and offsets accordingly

--- ex1-21.c
+++ ex1-21.c
@@ -41,14 +41,46 @@ int line_len(char line[], int lim){
   return i;
 }
 
+struct data{
+  int spaces;
+  int space_locations[100];
+
+}
+
 void entab(char to[], char from[], int len){
   int i;
+  int is_space;
+  int spaces;
+  int offset;
 
   // printf("--------\n%s\n", from);
 
   i = 0;
+  is_space = 0;
+  spaces = 0;
+  offset = 0;
   while(i < len){
+    if(spaces == MAXTAB){
+      offset += MAXTAB-1;
+      to[i-offset] = '\t';
+    }
 
+    if(from[i] == ' ' && is_space == 0){
+      is_space = 1;
+      ++spaces;
+      // to[i] = from[i];
+      printf("++spaces, spaces = %d", spaces);
+    }
+    else if(from[i] == ' ' && space == 1){
+      ++spaces
+      printf("++spaces, spaces = %d", spaces);
+    }
+    else if(from[i] != ' '){
+      is_space = 0;
+      spaces = 0;
+    }
+
+    ++i;
   }
 
   printf("original line: %s\nentabbed line: %s\n", from, to);
