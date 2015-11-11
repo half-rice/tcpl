f87a17910ce2d8cb422bad002d51edf6a9bb1d36
surrus <xsurrus@gmail.com>
2015-11-10 23:55:15 -0700

    removed structs

--- s2.c
+++ s2.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
 #include "SDL2.h"
 
-struct man{
-  int x, y;
-  short life;
-  char *name;
-};
-
-void structs(){
-  man man;
-  man.x = 0;
-  man.y = 0;
-  man.life = 100;
-  man.name = "name";
-}
-
 int main(){
 
   return 0;
