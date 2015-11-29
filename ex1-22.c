58a1550c141494b3a3ce23c07a730b0c5777c8c6
surrus <xsurrus@gmail.com>
2015-11-28 21:18:59 -0700

    changed var names to make more sense. line? wtf gtfo

--- ex1-22.c
+++ ex1-22.c
@@ -3,7 +3,7 @@
 #define MAXSTRING 1000
 #define MAXLINE 100
 
-int line_len(char line[], int lim);
+int line_len(char string[], int lim);
 
 /* Write a program to "fold" long input lines into two or more shorter lines after 
 the last non-blank character that occurs before the n-th column of input. Make 
@@ -11,17 +11,18 @@ sure your program does something intelligent with very long lines, and if there
 are no blanks or tabs before the specified column */
 int main(){
   int len;
-  char line[MAXSTRING];
+  char string[MAXSTRING];
   char word[MAXLINE];
-  
-  while((len = line_len(line, MAXSTRING)) > 0){
+
+  while((len = string_len(string, MAXSTRING)) > 0){
+    printf("%s\n")
 
   }
 
   return 0;
 }
 
-int line_len(char line[], int lim){
+int string_len(char string[], int lim){
   int c, i;
 
   for(i = 0; i < lim-1 && (c=getchar()) != EOF && c != '\n'; ++i){
