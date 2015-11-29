fe7ca78a19fc227791eefc441d4928d2d072827d
surrus <xsurrus@gmail.com>
2015-11-29 02:07:37 -0700

    fold() sets cursor point and iterates backwards to find the last word of the line

--- ex1-22.c
+++ ex1-22.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 #define MAXSTRING 1000
-#define MAXLINE 100
+#define MAXLINE 7
 
-int line_len(char string[], int lim);
+int string_len(char string[], int lim);
 void fold(char to[], char from[], int len);
 
 /* Write a program to "fold" long input lines into two or more shorter lines after 
@@ -17,34 +17,72 @@ int main(){
   char word[MAXLINE];
 
   while((len = string_len(string, MAXSTRING)) > 0){
-    printf("%s\n");
-    if(len > MAXLINE){
+    printf("%s\n", string);
+    fold(fold_string, string, len);
+    /*if(len > MAXLINE){
       fold(fold_string, string, len);
     }
     else{
-      
-    }
+
+    }*/
   }
 
   return 0;
 }
 
-int fold(char to[], char from[], int len){
+int string_len(char string[], int lim){
+  int c, i;
+
+  for(i = 0; i < lim-1 && (c=getchar()) != EOF && c != '\n'; ++i){
+    string[i] = c;
+  }
+  if(c == '\n'){
+    string[i] = c;
+    ++i;
+  }
+
+  string[i] = '\0';
+  return i;
+}
+
+void fold(char to[], char from[], int len){
   int i;
   int c;
+  int x;
+  int cursor;
   int inword = 0;
+
+  cursor = 0;
+  if(from[MAXLINE-1] > 33 && from[MAXLINE-1] < 126 && from[MAXLINE-1] != '\0'){
+    inword = 1;
+
+    x = 0;
+    while(inword == 1){
+      if(from[(MAXLINE-1)-x] == ' '){
+        inword = 0;
+        cursor = (MAXLINE-1)-x;
+        break;
+      }
+
+      ++x;
+    }
+
+    while(from[cursor-x]
+  }
 
   i = 0;
   while(i < len){
-    printf("%c", from[i]);
-    if(from[i] > 33 || from[i] < 126 || from[i] != '\0'){
-      printf(" >\n");
+    // printf("%c", from[i]);
+    if(from[i] > 33 && from[i] < 126 && from[i] != '\0'){
+      // printf(" >");
     }
 
+    printf("\n");
     ++i;
   }
 }
 
+/*
 int word_len(char word[], int lim){
   int c, i;
 
@@ -54,18 +92,5 @@ int word_len(char word[], int lim){
 
   return i;
 }
+*/
 
-int string_len(char string[], int lim){
-  int c, i;
-
-  for(i = 0; i < lim-1 && (c=getchar()) != EOF && c != '\n'; ++i){
-    line[i] = c;
-  }
-  if(c == '\n'){
-    line[i] = c;
-    ++i;
-  }
-
-  line[i] = '\0';
-  return i;
-}
