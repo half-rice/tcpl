1ca3d79f7a5143bcde823f57d5f8eb3ac4a9b625
surrus <xsurrus@gmail.com>
2015-11-28 22:19:31 -0700

    more scaffolding

--- ex1-22.c
+++ ex1-22.c
@@ -4,6 +4,7 @@
 #define MAXLINE 100
 
 int line_len(char string[], int lim);
+void fold(char to[], char from[], int len);
 
 /* Write a program to "fold" long input lines into two or more shorter lines after 
 the last non-blank character that occurs before the n-th column of input. Make 
@@ -12,14 +13,46 @@ are no blanks or tabs before the specified column */
 int main(){
   int len;
   char string[MAXSTRING];
+  char fold_string[MAXSTRING];
   char word[MAXLINE];
 
   while((len = string_len(string, MAXSTRING)) > 0){
-    printf("%s\n")
-
+    printf("%s\n");
+    if(len > MAXLINE){
+      fold(fold_string, string, len);
+    }
+    else{
+      
+    }
   }
 
   return 0;
+}
+
+int fold(char to[], char from[], int len){
+  int i;
+  int c;
+  int inword = 0;
+
+  i = 0;
+  while(i < len){
+    printf("%c", from[i]);
+    if(from[i] > 33 || from[i] < 126 || from[i] != '\0'){
+      printf(" >\n");
+    }
+
+    ++i;
+  }
+}
+
+int word_len(char word[], int lim){
+  int c, i;
+
+  for(i = 0; i < lim-1; ++i){
+
+  }
+
+  return i;
 }
 
 int string_len(char string[], int lim){
