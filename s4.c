837bbc7eb3de3d21aff23eaa54a40a3b54e86a81
surrus <xsurrus@gmail.com>
2015-12-15 19:38:06 -0700

    s4.c

--- s4.c
+++ s4.c
@@ -2,20 +2,22 @@
 
 void returnArray(const char *srcArray, size_t srcSize, char *dstArray, char dstSize)
 {
-  // printf("*dstArray = %s, dstSize = %zd\n", dstArray, dstSize);
+  printf("*dstArray = %s, dstSize = %zd\n", dstArray, dstSize);
   int i;
   for(i = 0; i < dstSize; ++i){
     dstArray[i] = srcArray[i];
   }
-  // printf("*dstArray = %s, dstSize = %zd\n", dstArray, dstSize);
+  printf("*dstArray = %s, dstSize = %zd\n", dstArray, dstSize);
 }
 
 void returnArray2(const char srcArray[], size_t srcSize, char dstArray[], char dstSize)
 {
+  printf("dstArray = %s, dstSize = %zd\n", dstArray, dstSize);
   int i;
   for(i = 0; i < dstSize; ++i){
     dstArray[i] = srcArray[i];
   }
+  printf("dstArray = %s, dstSize = %zd\n", dstArray, dstSize);
   dstArray[0] = '>';
 }
 
@@ -24,8 +26,10 @@ int main()
   char src[] = "what the fuck";
   char dst[sizeof(src)];
 
-  // returnArray(src, sizeof(src), dst, sizeof(dst));
+  printf("\nreturnArray2()\n");
   returnArray2(src, sizeof(src), dst, sizeof(dst));
+  printf("returnArray()\n");
+  returnArray(src, sizeof(src), dst, sizeof(dst));
   printf("dst[] = %s\n", dst);
 
   return 0;
